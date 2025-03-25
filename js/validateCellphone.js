ea5db93c2a3e506b55fc18747880bd8c9c896362
Guilherme Reis <guireis1164@gmail.com>
2025-03-25 19:06:14 -0300

    Fetch(JavaScript): Implement format Cell phone function to format phone number in real time

--- js/validateCellphone.js
+++ js/validateCellphone.js
@@ -17,3 +17,29 @@ export function formatDDI() {
       return
     })
 }
+
+export function formatCellphone() {
+  document.getElementById("cellphone").addEventListener("input", function (ev) {
+    let value = ev.target.value.replace(/\D/g, "")
+
+    if (value.length > 11) {
+      value = value.slice(0, 11)
+    }
+
+    let formattedValue = ""
+    if (value.length > 0) {
+      formattedValue += "(" + value.substring(0, 2)
+      if (value.length > 2) {
+        formattedValue += ") "
+        formattedValue += value.substring(2, 7)
+        if (value.length > 7) {
+          formattedValue += "-" + value.substring(7, 11)
+        }
+      } else if (value.length === 2) {
+        formattedValue += ")"
+      }
+    }
+
+    ev.target.value = formattedValue
+  })
+}
