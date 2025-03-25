4e1aee2991392408524c06dfed5b832fc8f11f0d
Guilherme Reis <guireis1164@gmail.com>
2025-03-25 07:46:19 -0300

    Feat(Form): Adds new fields to the form, including detailed address, phone number and country code

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-const inputName = document.getElementById("name")
+const inputfirstName = document.getElementById("firstName")
+const inputLastName = document.getElementById("lastName")
+const inputCPF = document.getElementById("CPF")
+const inputCountryCode = document.getElementById("countryCode")
 const inputCellphone = document.getElementById("cellphone")
 const inputEmail = document.getElementById("email")
+const inputCountry = document.getElementById("country")
+const inputState = document.getElementById("state")
+const inputCity = document.getElementById("city")
+const inputCEP = document.getElementById("CEP")
+const inputDistrict = document.getElementById("district")
 const inputAddress = document.getElementById("address")
-const inputCPF = document.getElementById("CPF")
+const inputAddressNumber = document.getElementById("addressNumber")
+const inputComplement = document.getElementById("Complement")
 const inputSenha = document.getElementById("senha")
 const inputConfirmSenha = document.getElementById("confirmSenha")
 let user = {}
@@ -10,11 +19,8 @@ let user = {}
 document
   .getElementById("btnSubmit")
   .addEventListener("click", async function () {
-    const Password = inputSenha
-    const confirmSenha = inputConfirmSenha
-
     try {
-      if (Password.value.trim() !== confirmSenha.value.trim()) {
+      if (inputSenha.value.trim() !== confirmSenha.value.trim()) {
         Promise.reject("As senhas nao conferem")
         confirmSenha.classList.add("error")
         return
@@ -23,12 +29,21 @@ document
       }
 
       user = {
-        Name: inputName.value.trim(),
+        FirstName: inputfirstName.value.trim(),
+        LastName: inputLastName.value.trim(),
+        CPF: inputCPF.value.trim(),
+        CountryCode: inputCountryCode.value.trim(),
         Cellphone: inputCellphone.value.trim(),
         Email: inputEmail.value.trim(),
+        Country: inputCountry.value.trim(),
+        State: inputState.value.trim(),
+        City: inputCity.value.trim(),
+        CEP: inputCEP.value.trim(),
+        District: inputDistrict.value.trim(),
         Address: inputAddress.value.trim(),
-        CPF: inputCPF.value.trim(),
-        Password: Password.value,
+        AddressNumber: inputAddressNumber.value.trim(),
+        Comment: inputComplement.value.trim(),
+        Senha: inputSenha.value.trim(),
       }
 
       const response = await fetch(
@@ -49,13 +64,22 @@ document
         )
       }
 
-      inputName.value = ""
+      inputfirstName.value = ""
+      inputLastName.value = ""
+      inputCPF.value = ""
+      inputCountryCode.value = ""
       inputCellphone.value = ""
       inputEmail.value = ""
+      inputCountry.value = ""
+      inputState.value = ""
+      inputCity.value = ""
+      inputCEP.value = ""
+      inputDistrict.value = ""
       inputAddress.value = ""
-      inputCPF.value = ""
       inputSenha.value = ""
       inputConfirmSenha.value = ""
+      inputAddressNumber.value = ""
+      inputComplement.value = ""
 
       console.log(user)
       users.push(user)
@@ -68,37 +92,38 @@ document
   })
 
 async function pesquiseCPF() {
-  const cpf = inputCPF.value.trim();
+  const cpf = inputCPF.value.trim()
 
   try {
     const response = await fetch(
       "https://67e05cc17635238f9aad538a.mockapi.io/api/v1/users"
     )
-    if (!response.ok) {
-      return Promise.reject("Erro ao buscar usuários!")
-    }
+    if (!response.ok) return Promise.reject("Erro ao buscar usuários!")
 
-    const users = await response.json();
-    const user = users.find(user => user.CPF === cpf);
+    const users = await response.json()
+    const user = users.find((user) => user.CPF === cpf)
 
-    if (!user) {
-      alert("CPF não encontrado!");
-      return;
-    }
+    if (!user) return Promise.reject("CPF não encontrado!")
 
-    inputName.value = user.Name;
-    inputCellphone.value = user.Cellphone;
-    inputEmail.value = user.Email;
-    inputAddress.value = user.Address;
-    inputCPF.value = user.CPF;
-    inputSenha.value = user.Senha;
-    inputConfirmSenha.value = "";
+    inputfirstName.value = user.FirstName
+    inputLastName.value = user.LastName
+    inputCPF.value = user.CPF
+    inputCountryCode.value = user.CountryCode
+    inputCellphone.value = user.Cellphone
+    inputEmail.value = user.Email
+    inputCountry.value = user.Country
+    inputState.value = user.State
+    inputCity.value = user.City
+    inputCEP.value = user.CEP
+    inputDistrict.value = user.District
+    inputAddress.value = user.Address
+    inputAddressNumber.value = user.AddressNumber
+    inputComplement.value = user.Comment
+    inputSenha.value = user.Senha
+    inputConfirmSenha.value = ""
 
-    document.getElementById("toggleSenha").style.display = "none";
-    
+    document.getElementById("toggleSenha").style.display = "none"
   } catch (error) {
-    console.error("Erro:", error.message);
-    alert("⚠️ Erro ao buscar CPF!");
+    console.error("Erro:", error.message)
   }
 }
-
